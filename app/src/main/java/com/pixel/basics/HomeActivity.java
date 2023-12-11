d7e9c795384d67f6dfd7271939aa1624f77b0e10
PixelHell97 <abakhet2010@gmail.com>
2023-12-11 14:41:06 +0200

    C39_Android

---
+++ app/src/main/java/com/pixel/basics/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.pixel.basics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec_login_screen);
+    }
+}
